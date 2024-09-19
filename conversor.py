613360aee37da5dafb49312431406c8a162160b2
atilaF03 <144128328+atilaF03@users.noreply.github.com>
2024-09-19 13:44:36 +0000

    atualizar

---
+++ conversor.py
@@ -0,0 +1,8 @@
+import os
+
+m = float(input("insira os m  "))
+os.system("cls||clear")
+
+cms = m * 100 
+
+print(f"os metros {m:.2f}, equivalem a: {cms:.2f}")
---
+++ media.py
@@ -0,0 +1,9 @@
+nota1 = float(input("insira a primeria nota "))
+nota2 = float(input("insira a primeria nota "))
+nota3 = float(input("insira a primeria nota "))
+nota4 = float(input("insira a primeria nota "))
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+
+print(f"sua média é{media}")
+
