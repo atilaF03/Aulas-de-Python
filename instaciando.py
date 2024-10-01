ad00a4ffc516c1847a9c6c779bd629ee47e8c3ec
atilaF03 <144128328+atilaF03@users.noreply.github.com>
2024-10-01 03:12:37 +0000

    atualizar

--- instaciando.py
+++ instaciando.py
@@ -1,3 +1,18 @@
 
 class Carro:
-    
+    def __init__(self, nome) -> str:
+        self.nome = nome
+    """funções  dentro de um metodo isso é importante"""
+    def acelerar(self):
+        print(f"{self.nome} está acelerando")
+
+
+
+fusca = Carro ("Fusca")
+print(fusca.nome)
+fusca.acelerar()
+
+
+celta = Carro ("Celta")
+print(celta.nome)
+celta.acelerar()
