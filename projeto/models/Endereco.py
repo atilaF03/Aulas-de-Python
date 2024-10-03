5cd84886b7ce035f057fbf3343eb8859525b7c1b
atilaF03 <144128328+atilaF03@users.noreply.github.com>
2024-10-03 13:06:16 +0000

    atualizar

--- projeto/models/Endereco.py
+++ projeto/models/Endereco.py
@@ -1,4 +1,6 @@
-from enums.UnidadeFederativa import UnidadeFederativa 
+import sys
+sys.path.append("/workspaces/Python-Orientado-a-Objetos/")
+from projeto.models.enums.UnidadeFederativa import UnidadeFederativa 
 
 class Endereco():
     def __init__(self, logradouro: str,numero:str,complemento:str,cep:str,cidade:str, uf: UnidadeFederativa) -> None:
@@ -7,7 +9,16 @@ class Endereco():
         self.complemento = complemento
         self.cep = cep 
         self.cidade = cidade
-        self.uf= UnidadeFederativa
+        self.uf= uf 
 
     def __str__(self) -> str:
-        pass
+        return (f"\no logradouro é: {self.logradouro}"
+                f"\no número é: {self.numero}"
+                f"\no complemento é {self.complemento}"
+                f"\no cep é {self.cep}"
+                f"\na cidade é {self.cidade}"
+                f"\n a unidade federativa é {self.uf}"
+                )
+
+endereco = Endereco("logradouro", "number","complemento", "cep","cidade",UnidadeFederativa.BAHIA.sigla)
+print(endereco)
